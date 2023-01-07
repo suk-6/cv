274623cc357c0ae6c233d6e50e5230deb4a04936
suk-6 <admin@suk.kr>
2023-01-07 21:15:25 +0900

    feat: extraction.py

---
+++ extraction.py
@@ -0,0 +1,27 @@
+import cv2
+
+video = cv2.VideoCapture('./video.mp4')
+
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = video.get(cv2.CAP_PROP_FPS)
+
+print("length :", length)
+print("width :", width)
+print("height :", height)
+print("fps :", fps)
+
+count = 0
+
+while (video.isOpened()):
+    ret, image = video.read()
+
+    if (int(video.get(1)) % 20 == 0):
+        print('Saved frame number : ' + str(int(video.get(1))))
+        count_str = format(count, '06')
+        cv2.imwrite("./images/frame_%s.PNG" % count_str, image)
+        #print('Saved frame%d.jpg' % count)
+        count += 1
+
+video.release()
