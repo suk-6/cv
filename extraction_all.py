d6d69be71575bb547718553e3ea11eadce8f4981
suk-6 <admin@suk.kr>
2023-01-19 23:07:13 +0900

    feat: delete if

---
+++ extraction_all.py
@@ -0,0 +1,25 @@
+import cv2
+
+video = cv2.VideoCapture('./video.mp4')
+
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = video.get(cv2.CAP_PROP_FPS)
+
+print("length :", length)
+print("width :", width)
+print("height :", height)
+print("fps :", fps)
+
+count = 0
+
+while (video.isOpened()):
+    ret, image = video.read()
+
+    print('Saved frame number : ' + str(int(video.get(1))))
+    count_str = format(count, '06')
+    cv2.imwrite("./images/frame_%s.PNG" % count_str, image)
+    count += 1
+
+video.release()
