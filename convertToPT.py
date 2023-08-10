452981fcc27f83c3e8a2b984ebcad1a4372e0ded
suk-6 <me@suk.kr>
2023-08-10 22:05:37 +0900

    fix: Support (jpg and png)

--- convertToPT.py
+++ convertToPT.py
@@ -2,14 +2,14 @@ import torch
 import os
 from PIL import Image
 
-def parse_yolo_annotation(txt_path, imageWidth, imageHeight):
-    with open(txt_path, 'r') as file:
+def parse_yolo_annotation(txtPath, imageWidth, imageHeight):
+    with open(txtPath, 'r') as file:
         lines = file.readlines()
     
     annotations = []
     
     for line in lines:
-        class_id, xCenter, yCenter, boxWidth, boxHeight = map(float, line.strip().split())
+        classId, xCenter, yCenter, boxWidth, boxHeight = map(float, line.strip().split())
         # YOLO 포맷의 좌표를 원래 이미지의 크기로 변환
         xCenter *= imageWidth
         yCenter *= imageHeight
@@ -17,27 +17,27 @@ def parse_yolo_annotation(txt_path, imageWidth, imageHeight):
         boxHeight *= imageHeight
         
         # 바운딩 박스 정보를 텐서로 변환
-        annotation = torch.tensor([xCenter, yCenter, boxWidth, boxHeight, class_id])
+        annotation = torch.tensor([xCenter, yCenter, boxWidth, boxHeight, classId])
         annotations.append(annotation)
     
     return annotations
 
-image_folder = 'images'  # 이미지 폴더 경로
-label_folder = 'labels'  # 라벨 폴더 경로
-output_path = 'export.pt'  # 저장할 .pt 파일 경로
+imageFolder = 'images'  # 이미지 폴더 경로
+labelFolder = 'labels'  # 라벨 폴더 경로
+outputPath = 'export.pt'  # 저장할 .pt 파일 경로
 
 data = []
 
-for image_name in os.listdir(image_folder):
-    if image_name.endswith('.jpg'):  # 이미지 파일일 경우
-        image_path = os.path.join(image_folder, image_name)
-        txt_path = os.path.join(label_folder, image_name.replace('.jpg', '.txt'))
+for imageName in os.listdir(imageFolder):
+    if imageName.endswith('.jpg') or imageName.endswith('.PNG'):  # 이미지 파일일 경우
+        imagePath = os.path.join(imageFolder, imageName)
+        txtPath = os.path.join(labelFolder, f"{imageName[:-4]}.txt")
         
-        if os.path.exists(txt_path):
-            image = Image.open(image_path)
-            image_width, image_height = image.size
+        if os.path.exists(txtPath):
+            image = Image.open(imagePath)
+            imageWidth, imageHeight = image.size
             
-            annotations = parse_yolo_annotation(txt_path, image_width, image_height)
+            annotations = parse_yolo_annotation(txtPath, imageWidth, imageHeight)
             
             data.append({
                 'image': image,
@@ -45,4 +45,4 @@ for image_name in os.listdir(image_folder):
             })
 
 # .pt 파일로 데이터 저장
-torch.save(data, output_path)
+torch.save(data, outputPath)
