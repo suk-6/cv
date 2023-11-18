16bb2aec0d15a9542a89f7d8d20af3ece59ea849
suk-6 <me@suk.kr>
2023-11-18 20:58:26 +0900

    Refactor: move file

--- classification2.py
+++ classification2.py
@@ -5,21 +5,25 @@ import shutil
 import math
 import random
 
-rootDir = "/home/woosuk/230506-files"
+rootDir = "."
 
 imageDir = f"{rootDir}/images"
 
-imageList = os.listdir(imageDir)
+imageList = [
+    file
+    for file in os.listdir(imageDir)
+    if file.lower().endswith(".jpg")
+    or file.lower().endswith(".png")
+    or file.lower().endswith(".jpeg")
+]
 
-targetList = [file[:-4] for file in imageList if file.endswith(".jpg")]
-
-imageCount = len(targetList)
+imageCount = len(imageList)
 
 num = random.sample(range(imageCount), imageCount)
 
 trainCount = math.floor(imageCount * 0.7)
 valCount = math.floor(imageCount * 0.2)
-testCount = math.floor(imageCount * 0.1)
+testCount = imageCount - trainCount - valCount
 
 train = []
 val = []
@@ -39,42 +43,33 @@ if not os.path.exists(f"{rootDir}/labels/val"):
 if not os.path.exists(f"{rootDir}/labels/test"):
     os.mkdir(f"{rootDir}/labels/test")
 
+
+def move(file, dir):
+    shutil.move(
+        f"{rootDir}/images/{file}",
+        f"{rootDir}/images/{dir}",
+    )
+    shutil.move(
+        f"{rootDir}/labels/{file.split('.')[0]}.txt",
+        f"{rootDir}/labels/{dir}",
+    )
+
+
 for i in range(trainCount):
-    FileName = targetList[num[i]]
-    shutil.move(
-        f"{rootDir}/images/{FileName}.jpg",
-        f"{rootDir}/images/train",
-    )
-    shutil.move(
-        f"{rootDir}/labels/{FileName}.txt",
-        f"{rootDir}/labels/train",
-    )
+    FileName = imageList[num[i]]
+    move(imageList[num[i]], "train")
     train.append(FileName)
 
 for i in range(valCount):
     i += trainCount
-    FileName = targetList[num[i]]
-    shutil.move(
-        f"{rootDir}/images/{FileName}.jpg",
-        f"{rootDir}/images/val",
-    )
-    shutil.move(
-        f"{rootDir}/labels/{FileName}.txt",
-        f"{rootDir}/labels/val",
-    )
+    FileName = imageList[num[i]]
+    move(imageList[num[i]], "train")
     val.append(FileName)
 
 for i in range(testCount):
     i += trainCount + valCount
-    FileName = targetList[num[i]]
-    shutil.move(
-        f"{rootDir}/images/{FileName}.jpg",
-        f"{rootDir}/images/test",
-    )
-    shutil.move(
-        f"{rootDir}/labels/{FileName}.txt",
-        f"{rootDir}/labels/test",
-    )
+    FileName = imageList[num[i]]
+    move(imageList[num[i]], "train")
     test.append(FileName)
 
 
